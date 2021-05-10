349bed9754701d73942b02f8e924964ef4945b54
ParvinTiks <83602988+ParvinTiks@users.noreply.github.com>
2021-05-10 15:01:07 +0300

    Update Chuck Norris.py

--- Chuck Norris.py
+++ Chuck Norris.py
@@ -5,3 +5,19 @@ päring = requests.get(aadress)
 vastus = päring.json()
 
 print(vastus["value"])
+
+tagasiside = input("Kas nali meeldis :D? - ")
+if tagasiside == "Jah":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+elif tagasiside == "jah":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+elif tagasiside == "jes":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+elif tagasiside == "Jes":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+elif tagasiside == "Hell yeah":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+elif tagasiside == "JAH":
+    print("Peabki meeldima! Chuck Norris on JUMAL!!")
+else:
+    print("Sinust, poeg, ei saa asja!")
